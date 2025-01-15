ee0e45f0f3b362ea3bfbb91832f1a33b5d957351
catgamecat <catgamecat@mail.ru>
2025-01-15 17:25:46 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+username = 'Михаил'
+title  = 'Сходить за продуктами'
+content = 'Молоко, бананы, гречка'
+status = 'Активный'
+created_date = '15-01-2025'
+issue_date = '16-01-2025'
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Cтатус:', status)
+print('Дата создания заметки:', created_date)
+print('Дата истечения заметки:', issue_date)
+
+
